1c629ec2c80f6d60602a9bcc5e54abfccc61ba1c
rushil.jayant@gmail.com <rushil.jayant@gmail.com>
2024-05-30 10:30:23 -0500

    Reset Frontend

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashboardNavbar from "./components/DashboardNavbar";
-
-export default function App() {
-  return (
-    <DashboardNavbar/>
-  );
-}
--- frontend/src/components/GrowFolioLogo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./GrowFolioLogo.css";
-
-export const GrowFolioLogo = () => (
-<p className="navBrand">GrowFolio</p>
-
-);
